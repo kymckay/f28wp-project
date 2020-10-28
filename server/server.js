f60ca4405c13d54f7c24aa9d2882341d1e26af87
Kyle Mckay <5459452+kymckay@users.noreply.github.com>
2020-10-28 18:16:30 +0000

    Set up static file serving with express

--- server/server.js
+++ server/server.js
@@ -1,2 +1,11 @@
-/* eslint-disable no-console */
-console.log('placeholder');
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Pages and assets stored statically in public folder
+app.use(express.static('public'))
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`)
+})
