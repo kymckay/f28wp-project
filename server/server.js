8f0b3738e0067fa63f957c04cfeda63b4fd50343
Kyle Mckay <5459452+kymckay@users.noreply.github.com>
2020-10-28 20:45:02 +0000

    Use path to resolve public folder
    Makes this code less likely to break when deployed.

--- server/server.js
+++ server/server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = 8080;
 
-// Pages and assets stored statically in public folder
-app.use(express.static('public'))
+// Files stored statically in public folder
+app.use(express.static(path.join(__dirname,'../public/')))
+
+app.get('/play', (req, res) => {
+    // TODO Return game page if logged in otherwise ask to play as guest
+    // if (loggedIn || guest_cookie_set) {
+    //     res.send(game_page);
+    // }
+    // res.send(guest_question_page);
+
+})
 
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`)
