2c5f134f311f6fb2df6c37701f45b66651ce08c1
Kyle Mckay <5459452+kymckay@users.noreply.github.com>
2020-11-09 21:22:57 +0000

    Implement lobby initiation events logic

--- server/classes/lobby.js
+++ server/classes/lobby.js
@@ -1,22 +1,56 @@
+const World = require('./world');
+
 class Lobby {
   constructor(io) {
-    this.id = Lobby.lobbyID++;
     this.io = io;
-    const size = 4000;
-    this.world = new world(size, size);
+
+    // ID will be used as room for sockets
+    this.id = `${Lobby.lobbyID++}`;
+
+    this.world = new World();
     this.inProgress = false;
     this.players = [];
   }
 
   join(playerSocket) {
     this.players.push(playerSocket);
-    this.world.addPlayer(playerSocket.id);
-    this.io.to(`${this.id}`).emit('joined lobby', this.id);
+
+    // Give the player a ship in the world
+    const ship = this.world.addPlayer(playerSocket.id);
+    playerSocket.emit('player setup', {
+      id: ship.id,
+      pos: ship.pos,
+      dir: ship.angle,
+    });
+
+    // Tell everyone in the room this player has joined
+    this.io.to(this.id).emit('joined lobby', this.id);
+    playerSocket.join(this.id); // join the room
   }
 
   startGame() {
     this.inProgress = true;
     this.world.startGame();
+
+    // TODO in world
+    // const asteroids = [];
+    // for (let i = 0; i < 10; i += 1) {
+    //   const ast = new Asteroid(
+    //     [Math.random() * 1000, Math.random() * 1000],
+    //     50 + Math.random() * 50
+    //   );
+    //   asteroids.push({
+    //     id: ast.id,
+    //     pos: ast.pos,
+    //     vel: ast.velocity,
+    //     size: ast.size,
+    //   });
+    // }
+
+    this.io.to(this.id).emit('game start', {
+      world: [this.world.width, this.world.height],
+      asteroids: [],
+    });
   }
 }
 Lobby.lobbyID = 0;
