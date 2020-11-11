1e132a92432481e1c6ef969d6455aa213f440e13
esefton1 <71329926+esefton1@users.noreply.github.com>
2020-11-11 16:24:53 +0000

    Update database.js
    changed from querying database to createing database

--- server/database.js
+++ server/database.js
@@ -8,11 +8,23 @@ var con = mysql.createConnection({
 });
 
 // Connect and query database
+/*
 con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
     con.query(sql,function(err,result) {
         if (err) throw err;
         console.log("Result: " + result);
+    });
+});
+*/
+
+// Connect and create database
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    con.query("CREATE DATABASE mydb",function(err,result) {
+        if (err) throw err;
+        console.log("Database created");
     });
 });
