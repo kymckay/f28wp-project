e7072b63b4c29f63c8849fde4bf1e75ab04afff0
esefton1 <71329926+esefton1@users.noreply.github.com>
2020-11-11 16:03:37 +0000

    Update database.js
    start of database creation

--- server/database.js
+++ server/database.js
@@ -1,1 +1,17 @@
+var mysql = require ('mysql');
 
+//creating a connection
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: ""
+});
+
+// Connect and create database
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("connected");
+    con.query(sql,function(err,result) {
+        if (err) throw err;
+        console.log("Database Dreated");
+});
