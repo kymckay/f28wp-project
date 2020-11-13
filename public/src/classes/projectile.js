f7c6b5aab60592fc5411f9c24d052bb0b417721b
esefton1 <71329926+esefton1@users.noreply.github.com>
2020-11-13 14:21:25 +0000

    Update projectile.js
    Adding experation time to projectiles and initial steps to removing them once expired.

--- public/src/classes/projectile.js
+++ public/src/classes/projectile.js
@@ -6,11 +6,27 @@ export default class Projectile extends Entity {
     this.angle = angle;
 
     this.element.classList.add('projectile');
+    
+    this.lastProjectile = performance.now();
   }
 
   render(screenOX, screenOY) {
+    if (isExpired) {
+      return null;
+    }
     super.render(screenOX, screenOY);
 
     this.element.style.transform = `translate(-50%, -50%) rotate(${this.angle}rad)`;
   }
 }
+
+//check if the projectile should expire
+isExpired() {
+  if (performance.now() - this.lastProjectile >= projectile.expirationTime){
+    return true;
+  }
+  return false;
+}
+
+//Constants for projectile behaviour
+projectiles.expirationTime = 5000; //ms for how long the projectiles exist
