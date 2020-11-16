19b4c29e1eeffaf605df846614921ef7f9f1055d
esefton1 <71329926+esefton1@users.noreply.github.com>
2020-11-16 21:53:00 +0000

    Update server.js
    More syntax changes

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 
 const path = require('path');
+
 const app = express();
 const port = 8070;
 const mysql = require('mysql');
 const alert = require('alert');
 
 const bodyParser = require('body-parser');
+
 const { check } = require('express-validator');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Files stored statically in public folder
@@ -16,11 +19,11 @@ app.use(express.static(path.join(__dirname, '../public/')));
 app.post('/play', (req, res) => {
 // TODO Check if guest name set, or logged in
 
-  res.sendFile(path.join(__dirname, '../public/play.html'))
+  res.sendFile(path.join(__dirname, '../public/play.html'));
 });
 
 // following block of code creates the database and its tables locally.
-//Must do it outside post to avoid overwrite.
+// Must do it outside post to avoid overwrite.
 const con = mysql.createConnection({
   host: 'localhost', // use your own hostname
   user: 'root', // use your mysql username
@@ -49,36 +52,35 @@ con.connect(function (err) {
 
   con.query(query1, function (err, result) {
     if (err) throw err;
-    console.log("Table created");
+    console.log('Table created');
   });
-
+    
 });
 
-
 app.post('/register', (req, res) => {
-    // TODO Register new user, check if already exists, handle client side after register
+  // TODO Register new user, check if already exists, handle client side after register
 
-    const name = req.body.user
-    const pass = req.body.pword
-    const query2 = "INSERT INTO players (username, password) VALUES (?, ?)";
+  const name = req.body.user;
+  const pass = req.body.pword;
+  const query2 = 'INSERT INTO players (username, password) VALUES (?, ?)';
 
-    con.connect(function(err) {
+  con.connect(function (err) {
 
-        con.query(query2, [name, pass], (err, results, fields) => {
-            if (err) {
-                console.log("Failed to register: " + err)
-                //res.send(<script>alert("Username already exists."); window.location.href = "index.html"; </script>);
-                res.sendStatus(500)
-                //res.render('index.html', {alertMsg:"Username already exists."}); //assuming only error possible is violation of primary key
-                return
+      con.query(query2, [name, pass], (err, results, fields) => {
+          if (err) {
+              console.log("Failed to register: " + err);
+              //res.send(<script>alert("Username already exists."); window.location.href = "index.html"; </script>);
+              res.sendStatus(500);
+              //res.render('index.html', {alertMsg:"Username already exists."}); //assuming only error possible is violation of primary key
+              return;
             }
                 console.log("Registered new user successfully.");
                 res.redirect("index.html");
                 res.end()
 
-        })
-    })
-})
+        });
+    });
+});
 
 app.post('/login', (req, res) => {
     // TODO Login existing user if exists, refuse otherwise
